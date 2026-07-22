777b19dce4ebe427ec6247b7bb235d4402213e4f
tanikipartnership <tanishkgupta98@gmail.com>
2026-07-23 03:08:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const openInvite = document.getElementById('openInvite');
 const music = document.getElementById('bgMusic');
 const musicBtn = document.getElementById('musicBtn');
 
-const MUSIC_START = -10; // seconds
+const MUSIC_START = 15; // seconds
 function seekToStart() {
   try { music.currentTime = MUSIC_START; } catch (e) { /* ignore */ }
 }
